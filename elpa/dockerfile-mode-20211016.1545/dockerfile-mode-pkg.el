cd2f1ac0b65d76d400fc2ceb0fb5c8e75baf5f08
shizhonghe <shi_zhonghe@163.com>
2022-01-12 05:02:33 +0800

    导出的笔记不包含子目录 "doki notebook"

---
+++ elpa/dockerfile-mode-20211016.1545/dockerfile-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from dockerfile-mode.el  -*- no-byte-compile: t -*-
+(define-package #("dockerfile-mode" 0 10 (font-lock-fontified t wrap-prefix #("    " 0 4 (face org-indent)) line-prefix #("    " 0 4 (face org-indent)) fontified t)) "20211016.1545" "Major mode for editing Docker's Dockerfiles" '((emacs "24")) :commit "5db94549ce8b000ae35adf511c820ad228178052" :keywords '("docker") :url "https://github.com/spotify/dockerfile-mode")
